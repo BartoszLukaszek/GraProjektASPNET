51abe1bbe884342fc7b234c029481af3b9799ff6
BartoszLukaszek <barti120@onet.pl>
2022-01-20 13:30:22 +0100

    Projekt

---
+++ Gra Projekt ASP.NET/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Gra_Projekt_ASP.NET.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gra_Projekt_ASP.NET.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
---
+++ Gra Projekt ASP.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Gra_Projekt_ASP.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gra_Projekt_ASP.NET.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
